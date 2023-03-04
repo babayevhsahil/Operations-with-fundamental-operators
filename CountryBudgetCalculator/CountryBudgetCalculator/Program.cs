cb9815f0cd50624c2d0aa94658fbc2d7b04d173a
Mirsail Babayev <mirsailsb@code.edu.az>
2023-03-04 17:48:44 +0400

    CountryBudgetCalculator

---
+++ CountryBudgetCalculator/CountryBudgetCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CountryBudgetCalculator
+{
+   internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Büdce: ");
+            double budget = double.Parse(Console.ReadLine());
+            double defenseSpending = budget * 0.20;
+            double techSpending = budget * 0.10;
+            double remainingBudget = budget - defenseSpending - techSpending;
+
+            Console.WriteLine("Mudafie Nazirliyine: " + defenseSpending);
+            Console.WriteLine("Yüksek Texnologiyalar Nazirliyine: " + techSpending);
+            Console.WriteLine("Yerde qalan budce: " + remainingBudget);
+        }
+    }
+
+}
