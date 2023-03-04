85fc69eb1ef85740d6f0489d04e0af63c7ec2051
Mirsail Babayev <mirsailsb@code.edu.az>
2023-03-04 17:30:56 +0400

    CountryBudgetCalculation

---
+++ country budget calculation/country budget calculation/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace country_budget_calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mebleg (AZN): ");
+            double amount = double.Parse(Console.ReadLine());
+            double usdRate = 1.6997;
+            double eurRate = 1.9366;
+            double rubRate = 0.0225;
+
+            Console.WriteLine("ABS dollari (USD): " + (amount / usdRate));
+            Console.WriteLine("Avro (EUR): " + (amount / eurRate));
+            Console.WriteLine("Rusiya rublu (RUB): " + (amount / rubRate));
+        }
+    }
+
+}
